525223f4ea4cb60e98d0c78b95e19e464d2bb0b9
tom4dev <tom4dev@gmail.com>
2013-03-12 23:08:48 +0100

    rajouts

--- fonctions.php
+++ fonctions.php
@@ -12,15 +12,15 @@ if( $fonction === "get_family" ){
 
 		$req="SELECT * FROM family;";
 		$result=$cnx->query($req);
-
+		$count = $result->rowCount(); 
 		
+		if($count>0){
 
-		if($result){
 			while($family = $result-> fetch(PDO::FETCH_ASSOC)){
-				echo '<option value="'.$family['fam_name'].'">'.ucfirst($family['fam_name']).'</option>';
+				echo '<option value="'.$family['fam_name'].'-'.$family['fam_world'].'">'.ucfirst($family['fam_name']).' ('.ucfirst($family['fam_world']).')</option>';
 			}
 			
-		}else{ echo'<option value="no_family"> Pas de famille </option>';}
+		}else{ echo'<option value="no_family">Veuillez créer au moins une famille </option>';}
 		
 		
 		$result->closeCursor();
@@ -38,10 +38,11 @@ if( $fonction === "add_family"){
 	$nbMax = $_POST['nbMax'];
 
 	//Test pour vérifier que le formulaire est complet
+	
 	if( (!empty($name)) && (!empty($world)) && (!empty($nbMax)) ){
 		
 			//On teste si une famille portant le même nom et résidant dans le même monde existe déja.
-			$req="SELECT * FROM family WHERE fam_name = '".$name."'";
+			$req="SELECT * FROM family WHERE fam_name = '".$name."' AND fam_world = '".$world."'";
 			$result=$cnx->query($req);
 			$count = $result->rowCount(); 
 			
@@ -49,9 +50,9 @@ if( $fonction === "add_family"){
 				echo'Une famille existe déja sous ce nom: <b>'.$name.'</b> dans ce monde (<b>'.$world.'</b>).';
 			}else{
 				//Si elle n'existe pas on insere alors la nouvelle famille.
-				$req="INSERT INTO family VALUES ('".$name."','".$world."','".$nbMax."');";
+				$req="INSERT INTO family (fam_name,fam_world,fam_nbMax) VALUES ('".$name."','".$world."','".$nbMax."');";
 				$result=$cnx->exec($req);
-				echo'La famille <b>"' .$name .'"</b> pouvant contenir <b>'.$nbMax.' membres</b>, à été ajouté dans le monde <b>"'.$world.'"</b>.';
+				echo'La famille <b>"' .$name .'"</b> pouvant contenir <b>'.$nbMax.' membre(s)</b>, à été ajouté dans le monde <b>"'.$world.'"</b>.';
 			}
 	}
 	else{echo'Formulaire incomplet !';}
@@ -68,14 +69,25 @@ if( $fonction === "add_monster"){
 	$age = $_POST['age'];
 	$family = $_POST['family'];
 
-	if( (!empty($name)) && (!empty($size)) && (!empty($age)) && (!empty($family)) ){
-		//On teste si un monstre portant le même nom, de la même famille et résidant dans le même monde existe déja.
+	//Image.
+	$_FILES['icone']['name']     //Le nom original du fichier, comme sur le disque du visiteur (exemple : mon_icone.png).
+	$_FILES['icone']['type']     //Le type du fichier. Par exemple, cela peut être « image/png ».
+	$_FILES['icone']['size']     //La taille du fichier en octets.
+	$_FILES['icone']['tmp_name'] //L'adresse vers le fichier uploadé dans le répertoire temporaire.
+	$_FILES['icone']['error']    //Le code d'erreur, qui permet de savoir si le fichier a bien été uploadé.
+	
+	if( $family === "no_family"){
+		echo'Veuillez <a href="addFamily.php">créer une famille</a>.';
+	}
+
+	if( (!empty($name)) && (!empty($size)) && (!empty($age)) && (!empty($family)) && ($family != "no_family") ){
+		//On teste si un monstre portant le même nom, la même famille et résidant dans le même monde existe déja.
 			$req="SELECT * FROM monsters WHERE mo_name = '".$name."' AND mo_family ='".$family."'";
 			$result=$cnx->query($req);
 			$count = $result->rowCount(); 
 			
 			if($count>0){
-				echo'Une monstre existe déja sous ce nom: <b>'.$name.'</b> dans cette famille (<b>'.$family.'</b>).';
+				echo'Un monstre existe déja sous ce nom: <b>'.$name.'</b> dans cette famille (<b>'.$family.'</b>).';
 			}else{
 				//Si il n'existe pas on l'insere alors dans la famille.
 				$req="INSERT INTO monsters VALUES ('".$name."','".$family."','".$size."','".$age."');";
@@ -84,6 +96,26 @@ if( $fonction === "add_monster"){
 			}
 	}else{echo'Formulaire incomplet !';}
 	$cnx = null; // Fermeture de la connexion
+}
+
+/*###### Get family ID ######*/
+function getFamilyID($family_name,$world_name){
+	
+		$req="SELECT * FROM family WHERE fam_name = '".$family_name."' AND fam_world = '".$world_name."' ;";
+		$result=$cnx->query($req);
+		$count = $result->rowCount(); 
+		
+		if($count>0){
+
+			while($family = $result-> fetch(PDO::FETCH_ASSOC)){
+				return $family['fam_id'];
+			}
+			
+		}else{ echo'Aucune famille ne correspond à la recherche.';}
+		
+		
+		$result->closeCursor();
+		$cnx = null; // Fermeture de la connexion
 }
 
 ?>
